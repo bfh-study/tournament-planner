360f689602015cf728d481045ad58899dbaee3ab
Samuel Ackermann <saemi.ackermann@gmail.com>
2018-01-15 22:41:05 +0100

    remove unused fields

---
+++ src/Migrations/Version20180115180156.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180115180156 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE teams DROP played, DROP won, DROP drawn, DROP lost, DROP goals_for, DROP goals_against');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE teams ADD played INT NOT NULL, ADD won INT NOT NULL, ADD drawn INT NOT NULL, ADD lost INT NOT NULL, ADD goals_for INT NOT NULL, ADD goals_against INT NOT NULL');
+    }
+}
